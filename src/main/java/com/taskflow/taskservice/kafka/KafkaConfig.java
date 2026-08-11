b24218492bb014025ee663766dc37ad91a7c796e
Afreen <mdafreen15042000@gmail.com>
2026-08-11 19:31:45 +0530

    dockerizing the services

--- src/main/java/com/taskflow/taskservice/kafka/KafkaConfig.java
+++ src/main/java/com/taskflow/taskservice/kafka/KafkaConfig.java
@@ -20,7 +20,7 @@ public class KafkaConfig {
     public ConsumerFactory<String, byte[]> consumerFactory() {
 
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "task-group");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
