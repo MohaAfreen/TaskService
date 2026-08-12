13488142c2411e02c1a25c6fc5e24a20782ae01b
Afreen <mdafreen15042000@gmail.com>
2026-08-12 16:09:51 +0530

    added api-gateway

--- src/main/java/com/taskflow/taskservice/security/JwtUtil.java
+++ src/main/java/com/taskflow/taskservice/security/JwtUtil.java
@@ -1,39 +1,57 @@
 package com.taskflow.taskservice.security;
 
-
 import io.jsonwebtoken.*;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
+
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
-    private final String SECRET="UniversalFavoriteAndSuperHandsomeSongjoongkiSecretKey12345";
 
-    public Claims extractClaims(String token){
-        return Jwts.parser()
-                .setSigningKey(SECRET)
+    private final String SECRET =
+            "UniversalFavoriteAndSuperHandsomeSongjoongkiSecretKey12345";
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(
+                SECRET.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public Claims extractClaims(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
 
     public boolean validateToken(String token) {
+
         try {
             Jwts.parserBuilder()
-                    .setSigningKey(SECRET) // your secret key
+                    .setSigningKey(getSigningKey())
                     .build()
-                    .parseClaimsJws(token); // validates signature + structure
+                    .parseClaimsJws(token);
 
             return !isTokenExpired(token);
 
         } catch (ExpiredJwtException e) {
             System.out.println("JWT expired");
+
         } catch (UnsupportedJwtException e) {
             System.out.println("Unsupported JWT");
+
         } catch (MalformedJwtException e) {
             System.out.println("Malformed JWT");
+
         } catch (SecurityException e) {
             System.out.println("Invalid signature");
+
         } catch (IllegalArgumentException e) {
             System.out.println("JWT is empty");
         }
@@ -42,9 +60,10 @@ public class JwtUtil {
     }
 
     private boolean isTokenExpired(String token) {
-        Date expiration = extractClaims(token).getExpiration();
+
+        Date expiration =
+                extractClaims(token).getExpiration();
+
         return expiration.before(new Date());
     }
-
 }
-
